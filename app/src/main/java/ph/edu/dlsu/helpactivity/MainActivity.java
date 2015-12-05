1a3dea910fba22dc0014b480fef5fca1ec18f255
KevinCepria <cepria21@yahoo.com>
2015-12-05 18:14:19 +0800

    Help activity

---
+++ app/src/main/java/ph/edu/dlsu/helpactivity/MainActivity.java
@@ -0,0 +1,78 @@
+package ph.edu.dlsu.helpactivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        InputStream iFile = getResources().openRawResource(R.raw.message);
+        try {
+            TextView helpText = (TextView) findViewById(R.id.TextView_HelpText);
+            String strFile = inputStreamToString(iFile);
+            helpText.setText(strFile);
+        } catch (Exception e) {
+            // Handle Exception, i.e. toast cannot open file
+        }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public String inputStreamToString(InputStream is) throws IOException {
+        StringBuffer sBuffer = new StringBuffer();
+        DataInputStream dataIO = new DataInputStream(is);
+        String strLine = null;
+
+        while ((strLine = dataIO.readLine()) != null) {
+            sBuffer.append(strLine + "\n");
+        }
+
+        dataIO.close();
+        is.close();
+
+        return sBuffer.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
